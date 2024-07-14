a084c72f1a4ff6e6da0a20d74bbe1c2b9fd565dc
Movindu Liyanage <142730691+Movin21@users.noreply.github.com>
2024-07-14 10:55:22 +0530

    Blog section Update

---
+++ components/BlogCard.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+const BlogCard = ({
+  title,
+  description,
+  date,
+  image,
+  href,
+}: {
+  title: string;
+  description: string;
+  date: string;
+  image: string;
+  href: string;
+}) => {
+  return (
+    <div className="relative overflow-hidden w-80 h-[28rem] rounded-2xl transition duration-200 group bg-black hover:shadow-xl border border-cyan-500 ">
+      <div className="w-full h-48 bg-gray-500 rounded-tr-lg rounded-tl-lg overflow-hidden relative">
+        <Image
+          src={image}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="group-hover:scale-95 group-hover:rounded-2xl transform object-cover transition duration-200"
+        />
+      </div>
+      <div className="p-4 flex flex-col justify-between h-[calc(100%-12rem)]">
+        <div>
+          <h2 className="font-bold mb-2 text-lg text-cyan-500 line-clamp-2">
+            {title}
+          </h2>
+          <p className="font-light text-sm text-white line-clamp-3">
+            {description}
+          </p>
+        </div>
+        <div className="flex flex-row justify-between items-center mt-4">
+          <span className="text-sm text-white">{date}</span>
+
+          <a
+            href={href}
+            className="relative z-10 px-4 py-2 bg-cyan-500 text-white font-bold rounded-xl block text-xs"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -1,25 +1,24 @@
-import { cn } from "@/utils/cn";
-import { FaMedium } from "react-icons/fa6";
 import React from "react";
 import Image from "next/image";
-import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
-import {
-  IconArrowWaveRightUp,
-  IconBoxAlignRightFilled,
-  IconBoxAlignTopLeft,
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
-} from "@tabler/icons-react";
-import { FaIcons } from "react-icons/fa6";
-import { Meteors } from "./ui/meteors";
+import { FaMedium } from "react-icons/fa6";
 import { BackgroundBeams } from "./ui/background-beams";
-import { FollowerPointerCard } from "./ui/following-pointer";
+import BlogCard from "./BlogCard";
+
 const Blogs = () => {
+  const featuredBlogs = [
+    {
+      title: "Introduction to Angular",
+      description:
+        "Angular is a popular open-source web application framework developed and maintained by Google. It is written in TypeScript, a superset of JavaScript, and is designed to simplify the development of complex web applications",
+      date: "July 14, 2024",
+      image: "/public/bg.png",
+      href: "/blog/featured-post-1",
+    },
+  ];
+
   return (
-    <div className="md:w-max h-full lg:w-full rounded-md relative flex flex-col items-center justify-center ">
-      <div className="mt-36">
+    <div className="md:w-max h-full lg:w-full rounded-md relative flex flex-col items-center justify-center">
+      <div className="mt-36 w-full ">
         <BackgroundBeams />
         <h1 className="pb-32 heading text-3xl sm:text-4xl lg:text-5xl text-center font-bold">
           Featured&nbsp;
@@ -28,22 +27,11 @@ const Blogs = () => {
             <FaMedium className="ml-2" />
           </span>
         </h1>
-        <div className="relative overflow-hidden h-full rounded-2xl transition duration-200 group bg-gradient-to-r from-cyan-500 to-blue-500 hover:shadow-xl border border-zinc-100">
-          <div className="w-80 mx-auto">
-            <div className=" p-4">
-              <h2 className="font-bold my-4 text-lg text-black">
-                {blogContent.title}
-              </h2>
-              <h2 className="font-normal my-4 text-sm text-white">
-                {blogContent.description}
-              </h2>
-              <div className="flex flex-row justify-between items-center mt-10">
-                <span className="text-sm text-white">{blogContent.date}</span>
-                <div className="relative z-10 px-6 py-2 bg-black text-white font-bold rounded-xl block text-xs">
-                  <a href="#">Read More</a>
-                </div>
-              </div>
-            </div>
+        <div className="flex justify-center w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-28 gap-y-8 justify-items-center">
+            {featuredBlogs.map((blog, index) => (
+              <BlogCard key={index} {...blog} />
+            ))}
           </div>
         </div>
       </div>
@@ -52,14 +40,3 @@ const Blogs = () => {
 };
 
 export default Blogs;
-
-const blogContent = {
-  slug: "amazing-tailwindcss-grid-layouts",
-  author: "Manu Arora",
-  date: "28th March, 2023",
-  title: "Amazing Tailwindcss Grid Layout Examples",
-  description:
-    "Grids are cool, but Tailwindcss grids are cooler. In this article, we will learn how to create amazing Grid layouts with Tailwindcs grid and React.",
-
-  authorAvatar: "/manu.png",
-};
