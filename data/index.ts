8178e2830ebf4c66268828cd01fe5ffeba5b4648
Movindu Liyanage <142730691+Movin21@users.noreply.github.com>
2024-07-19 15:43:11 +0530

    depoly error fix 7

--- data/index.ts
+++ data/index.ts
@@ -4,7 +4,7 @@ export const featuredBlogs = [
     description:
       "Angular is a popular open-source web application framework developed and maintained by Google. It is written in TypeScript, a superset of JavaScript, and is designed to simplify the development of complex web applications",
     date: "July 14, 2024",
-    image: "/Angular Blog 1.png",
+    image: "Angular Blog 1.png",
     href: "https://medium.com/@movinliyanage/exploring-angular-an-undergraduate-developers-perspective-3bf210ffa966",
   },
 ];
@@ -14,7 +14,7 @@ export const projects = [
     id: 1,
     title: "EventNest - Online Event Planning System",
     des: "Led the development of EventNest, a full-stack web application for event planning, utilizing MVC architecture and OOP principles. The platform caters to multiple user roles including admins, event managers, attendees, and service providers.",
-    img: "/eventnest.svg",
+    img: "eventnest.svg",
     iconLists: ["/java.svg"],
     linkTitle: "GitHub.com/Movin21",
     link: "https://github.com/Movin21/Online-Event-Planning-System",
@@ -24,7 +24,7 @@ export const projects = [
     id: 2,
     title: "HirelyAI- Job Applicant Management System",
     des: "This is an job applicant management system designed to streamline the hiring process for both applicants and employers. The system incorporates advanced functionalities to enhance user experience and efficiency in the hiring process.",
-    img: "/HirelyAi.svg",
+    img: "HirelyAi.svg",
     iconLists: [
       "/re.svg",
       "/tail.svg",
@@ -40,7 +40,7 @@ export const projects = [
     id: 3,
     title: "RelicRoom-Online Auction Management System",
     des: "This project provides a comprehensive platform for vintage item auctions, connecting auctioneers, bidders, vintage experts, and repair specialists in a seamless environment. ",
-    img: "/RelicRoom.svg",
+    img: "RelicRoom.svg",
     iconLists: [
       "/re.svg",
       "/tail.svg",
@@ -56,7 +56,7 @@ export const projects = [
     id: 4,
     title: "@movin.dev- Developer Portfolio",
     des: "You are Currently Experiencing My Developer Portfolio.",
-    img: "/movindev.svg",
+    img: "movindev.svg",
     iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
     linkTitle: "www.movin.dev",
     link: "www.movin.dev",
